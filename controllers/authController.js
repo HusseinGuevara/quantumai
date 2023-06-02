d68474000097e3453cba2d4f8655683733a3374b
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2023-06-01 21:26:10 -0700

    Add files via upload

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,91 @@
 const User = require('./../models/userModel.js');
+const AppError = require('./../utils/appError.js');
 const catchAsync = require('./../utils/catchAsync.js');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+// This will create a JWT token for the user
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
+};
+
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+    const cookiesOptions = {
+            expires: new Date(
+                Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 *1000
+            ),
+            //  Cookie can not be accessed or modified by browser. To prevent cross-side scripting attacks
+            httpOnly: true
+        };
+    // The cookie can only be sent in a encrypyted connection. When using https    
+    if(process.env.NODE_ENV === 'production') cookiesOptions.secure = true;
+    res.cookie('jwt', token, cookiesOptions);
+
+    // Remove password from output
+    user.password = undefined;
+
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    });
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
+    createSendToken(newUser, 201, res);
+});
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            newUser
-        }
-    });
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password} = req.body;
+
+    // 1) Check if email and password exist
+    if(!email || !password) {
+        return next(new AppError('Please provide email and password!'), 400);
+    }
+    // 2) Check if user exist and password is correct 
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user || !(await user.correctPassword( password, user.password))) {
+        return next(new AppError('Incorrect email or password.', 401));
+    }
+    
+    // 3) If everything is ok, send token to client
+    createSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync( async (req, res, next) => {
+    // 1) Getting token and check if its there
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    };
+
+    if(!token) {
+        return next(
+            new AppError('You are not logged in! Please log in to get access.', 401));
+    };
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+    const freshUser = await User.findById(decoded.id);
+    if(!freshUser) {
+        return next(new AppError('The user belonging to this token no longer exist!', 401));
+    };
+
+    // 4) Check if user changed password after the token was issued
+    if(freshUser.changedPasswordAfter(decoded.iat)) {
+        return next(new AppError('User recently changed password! Please log in again.', 401));
+    };
+
+    // Grant access to protected route
+    req.user = freshUser;
+    next();
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 
 const userSchema = new mongoose.Schema({
@@ -32,6 +33,42 @@ const userSchema = new mongoose.Schema({
         }   
     }
 });
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword,
+    userPassword
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function(JWTTimeStamp) {
+    if(this.passwordChangedAt) {
+        const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+        return JWTTimeStamp < changedTimestamp;
+    }
+
+    return false;
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+    // creates an unencrypted reset token
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+
+    this.passwordResetToken = crypto 
+        // sha256 generates an unique 256-bit (32-byte) signature
+        .createHash('sha256')
+        // this is the string that we want to hash
+        .update(resetToken)
+        // digest converts it to hexadecimal
+        .digest('hex');
+
+        console.log({ resetToken }, this.passwordResetToken);
+    
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+};
 
 const User = mongoose.model('User', userSchema);
 
